05c1c4ed9a65455aca143b6d45d8d619755b1ffc
lwstar <373102227@qq.com>
2018-10-25 19:31:58 +0800

    Create test.lua

---
+++ crossluagame/test.lua
@@ -0,0 +1,292 @@
+
+function printLog(tag, fmt, ...)
+    local t = {
+        "[",
+        string.upper(tostring(tag)),
+        "] ",
+        string.format(tostring(fmt), ...)
+    }
+    print(table.concat(t))
+end
+
+function printError(fmt, ...)
+    printLog("ERR", fmt, ...)
+    print(debug.traceback("", 2))
+end
+
+function printInfo(fmt, ...)
+    if type(DEBUG) ~= "number" or DEBUG < 2 then return end
+    printLog("INFO", fmt, ...)
+end
+
+local function dump_value_(v)
+    if type(v) == "string" then
+        v = "\"" .. v .. "\""
+    end
+    return tostring(v)
+end
+
+function dump(value, description, nesting)
+    if type(nesting) ~= "number" then nesting = 3 end
+
+    local lookupTable = {}
+    local result = {}
+
+    local traceback = string.split(debug.traceback("", 2), "\n")
+    print("dump from: " .. string.trim(traceback[3]))
+
+    local function dump_(value, description, indent, nest, keylen)
+        description = description or "<var>"
+        local spc = ""
+        if type(keylen) == "number" then
+            spc = string.rep(" ", keylen - string.len(dump_value_(description)))
+        end
+        if type(value) ~= "table" then
+            result[#result +1 ] = string.format("%s%s%s = %s", indent, dump_value_(description), spc, dump_value_(value))
+        elseif lookupTable[tostring(value)] then
+            result[#result +1 ] = string.format("%s%s%s = *REF*", indent, dump_value_(description), spc)
+        else
+            lookupTable[tostring(value)] = true
+            if nest > nesting then
+                result[#result +1 ] = string.format("%s%s = *MAX NESTING*", indent, dump_value_(description))
+            else
+                result[#result +1 ] = string.format("%s%s = {", indent, dump_value_(description))
+                local indent2 = indent.."    "
+                local keys = {}
+                local keylen = 0
+                local values = {}
+                for k, v in pairs(value) do
+                    keys[#keys + 1] = k
+                    local vk = dump_value_(k)
+                    local vkl = string.len(vk)
+                    if vkl > keylen then keylen = vkl end
+                    values[k] = v
+                end
+                table.sort(keys, function(a, b)
+                    if type(a) == "number" and type(b) == "number" then
+                        return a < b
+                    else
+                        return tostring(a) < tostring(b)
+                    end
+                end)
+                for i, k in ipairs(keys) do
+                    dump_(values[k], k, indent2, nest + 1, keylen)
+                end
+                result[#result +1] = string.format("%s}", indent)
+            end
+        end
+    end
+    dump_(value, description, "- ", 1)
+
+    for i, line in ipairs(result) do
+        print(line)
+    end
+end
+
+function printf(fmt, ...)
+    print(string.format(tostring(fmt), ...))
+end
+
+function checknumber(value, base)
+    return tonumber(value, base) or 0
+end
+
+function checkint(value)
+    return math.round(checknumber(value))
+end
+
+function checkbool(value)
+    return (value ~= nil and value ~= false)
+end
+
+function checktable(value)
+    if type(value) ~= "table" then value = {} end
+    return value
+end
+
+function isset(hashtable, key)
+    local t = type(hashtable)
+    return (t == "table" or t == "userdata") and hashtable[key] ~= nil
+end
+
+local setmetatableindex_
+setmetatableindex_ = function(t, index)
+    if type(t) == "userdata" then
+        local peer = tolua.getpeer(t)
+        if not peer then
+            peer = {}
+            tolua.setpeer(t, peer)
+        end
+        setmetatableindex_(peer, index)
+    else
+        local mt = getmetatable(t)
+        if not mt then mt = {} end
+        if not mt.__index then
+            mt.__index = index
+            setmetatable(t, mt)
+        elseif mt.__index ~= index then
+            setmetatableindex_(mt, index)
+        end
+    end
+end
+setmetatableindex = setmetatableindex_
+
+function clone(object)
+    local lookup_table = {}
+    local function _copy(object)
+        if type(object) ~= "table" then
+            return object
+        elseif lookup_table[object] then
+            return lookup_table[object]
+        end
+        local newObject = {}
+        lookup_table[object] = newObject
+        for key, value in pairs(object) do
+            newObject[_copy(key)] = _copy(value)
+        end
+        return setmetatable(newObject, getmetatable(object))
+    end
+    return _copy(object)
+end
+
+function class_create(classname, ...)
+    local cls = {__cname = classname}
+
+    local supers = {...}
+    for _, super in ipairs(supers) do
+        local superType = type(super)
+        assert(superType == "nil" or superType == "table" or superType == "function",
+            string.format("class() - create class \"%s\" with invalid super class type \"%s\"",
+                classname, superType))
+
+        if superType == "function" then
+            assert(cls.__create == nil,
+                string.format("class() - create class \"%s\" with more than one creating function",
+                    classname));
+            -- if super is function, set it to __create
+            cls.__create = super
+        elseif superType == "table" then
+            if super[".isclass"] then
+                -- super is native class
+                assert(cls.__create == nil,
+                    string.format("class() - create class \"%s\" with more than one creating function or native class",
+                        classname));
+                cls.__create = function() return super:create() end
+            else
+                -- super is pure lua class
+                cls.__supers = cls.__supers or {}
+                cls.__supers[#cls.__supers + 1] = super
+                if not cls.super then
+                    -- set first super pure lua class as class.super
+                    cls.super = super
+                end
+            end
+        else
+            error(string.format("class() - create class \"%s\" with invalid super type",
+                        classname), 0)
+        end
+    end
+
+    cls.__index = cls
+    if not cls.__supers or #cls.__supers == 1 then
+        setmetatable(cls, {__index = cls.super})
+    else
+        setmetatable(cls, {__index = function(_, key)
+            local supers = cls.__supers
+            for i = 1, #supers do
+                local super = supers[i]
+                if super[key] then return super[key] end
+            end
+        end})
+    end
+
+    if not cls.ctor then
+        -- add default constructor
+        cls.ctor = function() end
+    end
+    cls.new = function(...)
+        local instance
+        if cls.__create then
+            instance = cls.__create(...)
+        else
+            instance = {}
+        end
+        setmetatableindex(instance, cls)
+        instance.class = cls
+        instance:ctor(...)
+        return instance
+    end
+    cls.create = function(_, ...)
+        return cls.new(...)
+    end
+
+    return cls
+end
+
+-- function class(classname, super)
+--     local superType = type(super)
+--     local cls
+
+--     --如果父类既不是函数也不是table则说明父类为空
+--     if superType ~= "function" and superType ~= "table" then
+--         superType = nil
+--         super = nil
+--     end
+
+--     --如果父类的类型是函数或者是C对象
+--     if superType == "function" or (super and super.__ctype == 1) then
+--         -- inherited from native C++ Object
+--         cls = {}
+
+--         --如果父类是表则复制成员并且设置这个类的继承信息
+--         --如果是函数类型则设置构造方法并且设置ctor函数
+--         if superType == "table" then
+--             -- copy fields from super
+--             for k,v in pairs(super) do cls[k] = v end
+--             cls.__create = super.__create
+--             cls.super    = super
+--         else
+--             cls.__create = super
+--             cls.ctor = function() end
+--         end
+
+--         --设置类型的名称
+--         cls.__cname = classname
+--         cls.__ctype = 1
+
+--         --定义该类型的创建实例的函数为基类的构造函数后复制到子类实例
+--         --并且调用子数的ctor方法
+--         function cls.new(...)
+--             local instance = cls.__create(...)
+--             -- copy fields from class to native object
+--             for k,v in pairs(cls) do instance[k] = v end
+--             instance.class = cls
+--             instance:ctor(...)
+--             return instance
+--         end
+
+--     else
+--         --如果是继承自普通的lua表,则设置一下原型，并且构造实例后也会调用ctor方法
+--         -- inherited from Lua Object
+--         if super then
+--             cls = {}
+--             setmetatable(cls, {__index = super})
+--             cls.super = super
+--         else
+--             cls = {ctor = function() end}
+--         end
+
+--         cls.__cname = classname
+--         cls.__ctype = 2 -- lua
+--         cls.__index = cls
+
+--         function cls.new(...)
+--             local instance = setmetatable({}, cls)
+--             instance.class = cls
+--             instance:ctor(...)
+--             return instance
+--         end
+--     end
+
+--     return cls
+-- end
